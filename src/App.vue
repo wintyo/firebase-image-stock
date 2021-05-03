bf5b3d665fffd36d8e1315615cea1ae01886a81a
wintyo <wintyo1024@gmail.com>
2021-05-03 09:01:15 +0900

    開発環境の用意

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+});
+</script>
+
+<style>
+#app {
+  margin-top: 60px;
+  color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
